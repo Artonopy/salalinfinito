bf57fec328ac931576108f8c9c05678df5b151f5
Artonopy <130484428+Artonopy@users.noreply.github.com>
2025-03-25 21:16:27 +0100

    Delete stackbit.config.ts

--- stackbit.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineStackbitConfig } from '@stackbit/types';
-
-export default defineStackbitConfig({
-    stackbitVersion: '~0.6.0',
-    nodeVersion: '18',
-    ssgName: 'nextjs',  // Change this based on your framework
-    devCommand: 'npm run dev', // Change this based on your framework
-});

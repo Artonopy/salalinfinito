af7db7db932554b78343dea3c4b41ad0fc21c5f5
Artonopy <130484428+Artonopy@users.noreply.github.com>
2025-03-28 14:43:23 +0100

    Update Reservation.tsx

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -10,6 +10,8 @@ const ReservationPage = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  <script src="https://widget.trustmary.com/tL1CGHy3S"></script>
+
   // Contact info details
   const contactInfo = [
     {
